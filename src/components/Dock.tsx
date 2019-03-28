01ee1680bda55eb26cdf241455e2d6db78a0c271
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-28 20:20:56 +0100

    Create Dock component

---
+++ src/components/Dock.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+import { default as classNames } from 'classnames';
+import { createStyles, withStyles, WithStyles } from '@material-ui/core';
+
+export const positions = () =>
+  createStyles({
+    bottomRight: {
+      position: 'absolute',
+      bottom: 0,
+      right: 0
+    }
+  });
+
+export type DockPosition = keyof ReturnType<typeof positions>;
+
+export type DockProps = WithStyles<typeof positions> &
+  React.HTMLAttributes<HTMLDivElement> & {
+    position: DockPosition;
+  };
+
+export const Dock = withStyles(positions)(
+  ({ classes, position, children, className, ...divProps }: DockProps) => (
+    <div className={classNames(classes[position], className)} {...divProps}>
+      {children}
+    </div>
+  )
+);

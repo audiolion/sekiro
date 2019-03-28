f55546d9e86079b5126005d8137b0d58b5ba4af7
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-28 17:26:43 +0100

    Add equipment tab layout

--- src/components/Equipment.tsx
+++ src/components/Equipment.tsx
@@ -1,4 +1,42 @@
 import * as React from 'react';
-import { Typography } from '@material-ui/core';
+import { WithStyles, withStyles } from '@material-ui/core';
 
-export const Equipment = () => <Typography>Equipment</Typography>;
+export const styles = () => ({
+  root: {
+    width: '100%',
+    display: 'grid',
+    gridColumnGap: '20px',
+    gridRowGap: '20px',
+    gridTemplate: `
+      "tools tools arts page page"
+      "items items jutsu page page"
+    `
+  },
+  tools: {
+    gridArea: 'tools'
+  },
+  items: {
+    gridArea: 'items'
+  },
+  arts: {
+    gridArea: 'arts'
+  },
+  jutsu: {
+    gridArea: 'jutsu'
+  },
+  page: {
+    gridArea: 'page'
+  }
+});
+
+export const Equipment = withStyles(styles)(
+  ({ classes }: WithStyles<typeof styles>) => (
+    <div className={classes.root}>
+      <div className={classes.tools}>Tools</div>
+      <div className={classes.items}>Items</div>
+      <div className={classes.arts}>Arts</div>
+      <div className={classes.jutsu}>Jutsu</div>
+      <div className={classes.page}>Page</div>
+    </div>
+  )
+);

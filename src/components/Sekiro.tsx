57bbef52fc517c3aa7aa4a3edaefa9a3a4ad14b0
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-28 14:28:21 +0100

    Add Screen component to Sekiro component

--- src/components/Sekiro.tsx
+++ src/components/Sekiro.tsx
@@ -1,4 +1,4 @@
 import * as React from 'react';
-import { Typography } from '@material-ui/core';
+import { Screen } from './Screen';
 
-export const Sekiro = () => <Typography>Hello World</Typography>;
+export const Sekiro = () => <Screen />;

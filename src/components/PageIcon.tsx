81b9a753b737ebea3f00439dcca1112dcf429e54
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-28 20:21:37 +0100

    Create PageIcons and PageIcon components

---
+++ src/components/PageIcon.tsx
@@ -0,0 +1,32 @@
+import * as React from 'react';
+import { default as classNames } from 'classnames';
+import { createStyles, withStyles, WithStyles } from '@material-ui/core';
+
+export const styles = createStyles({
+  pageIcon: {
+    width: 25,
+    height: 30,
+    boxShadow: `0px 0px 5px 1px rgba(0,0,0,0.75)`,
+    background: 'gray'
+  },
+  active: {
+    background: 'white'
+  }
+});
+
+export type PageIconProps = WithStyles<typeof styles> &
+  React.HTMLAttributes<HTMLDivElement> & {
+    active?: boolean;
+  };
+
+export const PageIcon = withStyles(styles)((props: PageIconProps) => {
+  const { className, classes, active, ...divProps } = props;
+  return (
+    <div
+      {...divProps}
+      className={classNames(classes.pageIcon, className, {
+        [classes.active]: active
+      })}
+    />
+  );
+});
---
+++ src/components/PageIcons.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+import { createStyles, withStyles, WithStyles } from '@material-ui/core';
+import { PageIcon } from './PageIcon';
+import { noop } from '../functions/noop';
+
+export const styles = createStyles({
+  pageIcons: {
+    display: 'flex'
+  }
+});
+
+export type PageIconsProps = WithStyles<typeof styles> & {
+  value?: number;
+  count?: number;
+  onChange?: (newIndex: number) => any;
+};
+
+export const PageIcons = withStyles(styles)((props: PageIconsProps) => {
+  const { value = 0, count = 1, onChange = noop, classes } = props;
+  let indicators: React.ReactNode = null;
+  for (let i = 0; i < count; i += 1) {
+    indicators = (
+      <>
+        {indicators}
+        <PageIcon active={i === value} onClick={() => onChange(i)} />
+      </>
+    );
+  }
+  return <div className={classes.pageIcons}>{indicators}</div>;
+});

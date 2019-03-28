e67720514a05e9764f4182196c070bf4d0a17716
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-28 13:04:31 +0100

    Use next version of mui Typography

--- src/components/Sekiro.tsx
+++ src/components/Sekiro.tsx
@@ -1,3 +1,4 @@
 import * as React from 'react';
+import { Typography } from '@material-ui/core';
 
-export const Sekiro = () => <>Hello World</>;
+export const Sekiro = () => <Typography>Hello World</Typography>;
--- src/state/AppState.ts
+++ src/state/AppState.ts
@@ -1,5 +1,13 @@
 import { createContext } from 'react';
+import { createMuiTheme } from '@material-ui/core';
+import { observable } from 'mobx';
 
 export class AppState {
-  static Context = createContext<AppState>(new AppState());
+  @observable theme = createMuiTheme({
+    typography: {
+      useNextVariants: true
+    }
+  });
+
+  static Context = createContext(new AppState());
 }

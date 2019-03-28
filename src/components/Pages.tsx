6679eda87a6815f33508ec7425e5c0191877d60e
Kasper Sandin <kasper.sandin@gmail.com>
2019-03-28 20:22:01 +0100

    Implement pagination behavior for Pages component

--- src/components/Pages.tsx
+++ src/components/Pages.tsx
@@ -1,8 +1,31 @@
 import * as React from 'react';
+import { Button } from '@material-ui/core';
+import { PageIcons } from './PageIcons';
+import { Dock } from './Dock';
+import { noop } from '../functions/noop';
 
 export type PagesProps = React.PropsWithChildren<object> &
-  React.HTMLAttributes<HTMLDivElement>;
+  Pick<React.HTMLAttributes<HTMLDivElement>, 'className'> & {
+    value?: number;
+    onChange?: (newIndex: number) => any;
+  };
 
-export const Pages = ({ children, ...divProps }: PagesProps) => (
-  <div {...divProps}>{children}</div>
-);
+export const Pages = (props: PagesProps) => {
+  const { children, value = 0, onChange = noop, ...divProps } = props;
+  const childrenArray = React.Children.toArray(children);
+  const currentPage = childrenArray[value];
+  const changeToNextPage = () => onChange((value + 1) % childrenArray.length);
+  return (
+    <div {...divProps} style={{ position: 'relative' }}>
+      {currentPage}
+      <Dock style={{ display: 'flex' }} position="bottomRight">
+        <Button onClick={changeToNextPage}>Toggle</Button>
+        <PageIcons
+          value={value}
+          count={childrenArray.length}
+          onChange={onChange}
+        />
+      </Dock>
+    </div>
+  );
+};
